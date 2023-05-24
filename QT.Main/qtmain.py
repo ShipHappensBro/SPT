54a208e6a74c1206bd99e4eca5838e882357b0cf
ShipHappensBro <ivanshulakov9@gmail.com>
2023-05-24 12:44:33 +0300

    24/05

--- QT.Main/qtmain.py
+++ QT.Main/qtmain.py
@@ -9,38 +9,51 @@ import subprocess
 import qrcode
 import copy
 import socket
+import random
+import string
+import sqlite3
 
 current_domain=subprocess.run(["powershell.exe", "(Get-CimInstance Win32_ComputerSystem).Domain"], stdout=subprocess.PIPE, text=True).stdout.strip()
 
 class MainWindow(QtWidgets.QMainWindow,mainwindow.Ui_MainWindow,AutorizationWin.Ui_AutorizationWindow):
 
     def __init__(self,parent=None):
-        con=QSqlDatabase.addDatabase('mainbase')
-        con.setDatabaseName('mainbase.db')
+        con=QSqlDatabase.addDatabase('QSQLITE')
+        con.setDatabaseName('inv.db')
         super(MainWindow,self).__init__()
         self.setupUi(self)
         self.qr_generator.clicked.connect(self.bc_inv)
         self.print_btn.clicked.connect(self.print)
+        self.con=sqlite3.connect('inv.db',5,1,'DEFERRED',True,sqlite3.Connection,True)
+        self.cur=self.con.cursor()
     def bc_inv(self):
 
-        self.info=self.number_workstation_btn.text()+" "+self.SecondName.text()
         qr=qrcode.QRCode(version=1,
                         error_correction=qrcode.constants.ERROR_CORRECT_L,
                         box_size=10,
                         border=4,
                         )
-        
-        qr.add_data(self.info)
+        self.rand=''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for n in range(10))
+        qr.add_data(self.rand)
         qr.make(fit=True)
         image = qr.make_image(fill_color="black",
                                back_color="white"
                                )
-        image.save(self.info+'.png')
-        self.QR.setPixmap(QtGui.QPixmap(self.info+'.png'))
-        self.print_btn.setEnabled(True)            
+        image.save(self.rand+'.png')
+        self.info=(self.SecondName.text(),
+                   self.number_workstation_btn.text(),
+                   self.department.currentText(),
+                   self.rand)
+        self.QR.setPixmap(QtGui.QPixmap(self.rand+'.png'))
+        self.print_btn.setEnabled(True)
+        try:
+            self.cur.execute("INSERT INTO inv (Name,Number,Department,QR) VALUES (?,?,?,?)",self.info)
+            self.con.commit()
+        finally:
+             print('Done')
     def print(self):
             try:
-                os.startfile(self.info+".png",
+                os.startfile(self.rand+".png",
                         "print")
             except:
                  pass
